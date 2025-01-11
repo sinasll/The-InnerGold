276295d83498c55f30ca911f8a9a00e3b7cd52df
sinasll <sinasalah6@gmail.com>
2025-01-11 12:28:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-import validCodes from 'config.js';
-
 document.getElementById('codeForm').addEventListener('submit', function(event) {
   event.preventDefault();
+
+  // Define the valid codes directly in the script
+  const validCodes = ['DASF4X', 'XAFAR4', 'SINA00', 'ESAM98', 'SINA44'];
+  
   const enteredCode = document.getElementById('confirmationCode').value.toUpperCase();
+  
   if (validCodes.includes(enteredCode)) {
     alert('Code accepted! You are one of us.');
     window.location.href = 'main.html';  
