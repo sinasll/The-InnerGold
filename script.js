e3a25186daa701f91e8369fea9adc4277bd28ebb
sinasll <sinasalah6@gmail.com>
2025-01-12 01:21:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.getElementById('codeForm').addEventListener('submit', function(event) {
 
   // Define the valid codes with associated account information
   const validCodes = {
-    'DASF4X': { accountName: 'User1', accountId: '1234' },
-    'XAF044': { accountName: 'User2', accountId: '5678' },
-    '044044': { accountName: 'User3', accountId: '91011' },
-    'SINA00': { accountName: 'User4', accountId: '1213' },
-    'ES1998': { accountName: 'User5', accountId: '1415' },
-    '040404': { accountName: 'User6', accountId: '1617' }
+    'DASF4X': { accountName: 'DAS', accountId: '1234' },
+    'XAF044': { accountName: 'XAF', accountId: '5678' },
+    '044044': { accountName: 'UN', accountId: '91011' },
+    'SINA00': { accountName: 'SINA', accountId: '1213' },
+    'ES1998': { accountName: 'ES', accountId: '1415' },
+    '040404': { accountName: 'UN', accountId: '1617' }
   };
   
   const enteredCode = document.getElementById('confirmationCode').value.toUpperCase();
