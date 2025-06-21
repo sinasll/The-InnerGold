3c206c86c78d7d2ed5b67a92baf0185b36d0f84e
sinasll <sinasalah6@gmail.com>
2025-06-21 13:49:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
   const codeValue = document.getElementById('confirmationCode').value.trim();
   const usersByCode = {
-    '044044': { accountName: 'sin',   code: 'sina00' },
-    'RycNO':  { accountName: 'das',   code: 'das444' },
-    'jAJXB':  { accountName: 'xafar', code: 'xaf444' },
-    'hinJs':  { accountName: 'UN',    code: 'TIG444' },
-    '1213':   { accountName: 'UN',    code: 'TIG$!2' },
-    'jxrCl':  { accountName: 'UN',    code: 'TIGs!n' }
+    'sina00': { accountName: 'sin',   code: 'sina00' },
+    'das444':  { accountName: 'das',   code: 'das444' },
+    'xafar4':  { accountName: 'xafar', code: 'xaf444' },
+    'tig000':  { accountName: 'UN',    code: 'TIG444' },
+    'tig444':   { accountName: 'UN',    code: 'TIG$!2' },
+    'tigtig':  { accountName: 'UN',    code: 'TIGs!n' }
   };
   if (usersByCode[codeValue]) {
     alert('Code accepted! You’re one of us.');
